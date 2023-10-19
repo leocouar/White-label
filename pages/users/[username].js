6de0e903f29e0125879d2e52642d5094230efbbc
EnzoCepeda <enzoocobhc@hotmail.com.ar>
2023-10-19 16:35:48 -0300

    CentrarPestañas

--- pages/users/[username].js
+++ pages/users/[username].js
@@ -53,25 +53,25 @@ const Username = ({userSession, billsOfUSer})  => {
             <div className="bg-gray-100 w-1/5 h-auto ">
                 <ul id="tabs" className="w-full sticky top-24">
                     <a name={`usuarios`} className="hidden sm:block" href="#first" onClick={handleClick}>
-                    <li  className={`font-semibold hover:bg-red-200 py-3 flex justify-center ${tabs.usuarios ? `bg-red-300` : ``}`}>
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                    <li  className={`font-semibold hover:bg-red-200 py-3 flex justify-center${tabs.usuarios ? `bg-red-300` : ``}`}>
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 ml-0" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
                         </svg>
                         Usuario
                     </li>
                     </a>
                     <a id="default-tab" className="hidden sm:block" name={`activity`} href="#second" onClick={handleClick}>
-                    <li  className={`hover:bg-red-200 py-3 font-semibold flex ${tabs.activity ? `bg-red-300` : ``} justify-center`}>
+                    <li  className={`hover:bg-red-200 py-3 font-semibold flex justify-center${tabs.activity ? `bg-red-300` : ``} justify-center`}>
                         
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 ml-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
                         </svg>
                         Actividad
                     </li>
                     </a>
                     <a name={`segurity`} className="hidden sm:block" href="#thirt" onClick={handleClick}>
-                    <li className={`hover:bg-red-200 py-3 font-semibold flex justify-center ${tabs.segurity ? `bg-red-300 py-3` : ``}`}>
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 " fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                    <li className={`hover:bg-red-200 py-3 font-semibold flex justify-center${tabs.segurity ? `bg-red-300 py-3` : ``}`}>
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 ml-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z" />
                         </svg>
                         Seguridad
