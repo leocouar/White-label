5cc9959dba0f34b98ed5095c666f6545b3f2808a
Alan Medina <medinaalangabriel@gmail.com>
2023-10-24 16:48:47 -0300

    Idea de prueba para buscador (Para nada funcional)

--- components/filter/FilterComponent.js
+++ components/filter/FilterComponent.js
@@ -1,24 +1,69 @@
-const FilterComponent = ({ filterText, onFilter, onClear }) => (
-    <> 
-        <input
-        className="w-full p-2 bg-gray-100 border border-purple-500 border-gray-200 rounded-lg"
-            id="search"
-            type="text"
-            placeholder="Buscar por id"
-            aria-label="Search Input"
-            value={filterText}
-            onChange={onFilter}
-        />
-        {
-            filterText
-                ?
-                <button className={"text-blue-500"} onClick={onClear}>
-                    &nbsp;&nbsp;Limpiar
-                </button>
-            :
-                <></>
-        }
-    </>
-);
+import React, { useState, useRef, useEffect } from 'react';
 
-export default FilterComponent
+const FilterComponent = ({ filterText, onFilter, onClear, columns }) => {
+  const [selectedValue, setSelectedValue] = useState(columns ? columns[0] : null);
+  const [inputValue, setInputValue] = useState(filterText);
+  const radioRefs = useRef({});
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+    onFilter(e);
+  };
+
+  const limpiar = () => {
+    setInputValue('');
+    onClear();
+  };
+
+  const handleRadioChange = (column) => {
+    setSelectedValue(column);
+  };
+
+  useEffect(() => {
+    if (selectedValue) {
+      radioRefs.current[selectedValue.value].checked = true;
+    }
+  }, [selectedValue]);
+
+  return (
+    <div id="filterComponent" className="w-full">
+      <input
+        className="w-full p-2 bg-gray-100 border border-purple-500 border-gray-200 rounded-lg"
+        id="search"
+        type="text"
+        placeholder={`Buscando por: ${selectedValue ? selectedValue.name : ''}...`}
+        aria-label="Search Input"
+        value={inputValue}
+        onChange={handleInputChange}
+      />
+      <div className="flex">
+        <div id="orderBy" className="flex items-center w-9/12">
+          {columns &&
+            columns.map((column) => (
+              <div key={column.value} className="flex items-center mr-4">
+                <input
+                  type="radio"
+                  name="option"
+                  value={column.value}
+                  ref={(el) => (radioRefs.current[column.value] = el)}
+                  checked={selectedValue && selectedValue.value === column.value}
+                  onChange={() => handleRadioChange(column)}
+                  className="mr-2"
+                />
+                <span>{column.name}</span>
+              </div>
+            ))}
+        </div>
+        {filterText && (
+          <div className="flex justify-end w-3/12">
+            <button className="text-blue-500" onClick={limpiar}>
+              Limpiar
+            </button>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default FilterComponent;
--- components/products/Products.js
+++ components/products/Products.js
@@ -1,28 +1,40 @@
-import FilterComponent from  "@/components/filter/FilterComponent";
-import DataTable  from "react-data-table-component";
+import FilterComponent from "@/components/filter/FilterComponent";
+import DataTable from "react-data-table-component";
 import Link from 'next/link'
 import { deleteProduct } from "services/productService";
-import {useState, useMemo} from "react";
+import { useState, useMemo } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {faEye, faEdit, faTrash, faTag, faInfo} from '@fortawesome/free-solid-svg-icons'
+import { faEye, faEdit, faTrash, faTag, faInfo } from '@fortawesome/free-solid-svg-icons'
 
-const Products = ({products}) => {
-    const [filterText, setFilterText]= useState ('')
-    const filteredItems = products.filter(item=> filterText.toLowerCase() == '' || filterText.includes(item.id));
+const Products = ({ products }) => {
+    const [filterText, setFilterText] = useState('')
+    const filteredItems = products.filter(item => filterText.toLowerCase() == '' || filterText.includes(item.id));
     console.log(products);
-    const [data,setData]= useState(filteredItems)
+    const [data, setData] = useState(filteredItems)
 
     const handleDelete = (rowId) => {
         const updatedData = data.filter(row => row.id !== rowId);
         setData(updatedData);
         deleteProduct(rowId)
-      };
+    };
+
+    const availableCols = [
+        { value: 'id', name: 'Id' },
+        { value: 'name', name: 'Nombre' },
+        { value: 'category', name: 'Categoria' },
+        { value: 'code', name: 'Codigo' },
+        { value: 'price', name: 'Precio' },
+        { value: 'stock', name: 'Stock' },
+        { value: 'points', name: 'Puntos' }
+      ];
+
+    console.log(availableCols);
 
 
     const columns = [
-            
+
         {
-            
+
             name: 'Id',
             selector: row => row.id,
             sortable: true,
@@ -33,57 +45,57 @@ const Products = ({products}) => {
             sortable: true
         },
         {
-            name:'Categoria',
+            name: 'Categoria',
             selector: row => row.category ? row.category.name : "Categoria",
             sortable: true
         },
         {
-            name:'Codigo',
+            name: 'Codigo',
             selector: row => row.code ? row.code : "Codigo",
             sortable: true
         },
         {
             name: 'Precio',
-            selector: row =>row.price ? row.price : "Precio",
-            sortable: true   
+            selector: row => row.price ? row.price : "Precio",
+            sortable: true
         },
         {
             name: 'Stock',
-            selector:row=>row.stock ? row.stock : "Stock",
+            selector: row => row.stock ? row.stock : "Stock",
             sortable: true
         },
         {
             name: 'Puntos',
-            selector:row=>row.points ? row.points : "Puntos",
+            selector: row => row.points ? row.points : "Puntos",
             sortable: true
         },
         {
             name: 'Acciones',
-            cell: (row) => ( 
+            cell: (row) => (
                 <div className="flex justify-center absolute left-0">
                     <Link
-                    href={`/products/${row.id}`}
-                    passHref
+                        href={`/products/${row.id}`}
+                        passHref
                     >
-                    <button onClick={() => console.log('Button clicked!')} className="bg-green-500 ml-0 hover:bg-green-400 text-white w-10 h-auto p-2 rounded-full font-primary font-semibold text-xs flex
+                        <button onClick={() => console.log('Button clicked!')} className="bg-green-500 ml-0 hover:bg-green-400 text-white w-10 h-auto p-2 rounded-full font-primary font-semibold text-xs flex
                          items-baselinetransform transition duration-500 group cursor-pointer">
-                    <FontAwesomeIcon icon={faEye} className="w-5 m-auto"/>
-                    </button>
+                            <FontAwesomeIcon icon={faEye} className="w-5 m-auto" />
+                        </button>
                     </Link>
 
                     <Link
-                    href={`/products/update/${row.id}`}
-                    passHref
+                        href={`/products/update/${row.id}`}
+                        passHref
                     >
-                    <button className="bg-blue-500 ml-0 hover:bg-blue-400 text-white w-10 h-auto p-2 rounded-full font-primary font-semibold text-xs flex
+                        <button className="bg-blue-500 ml-0 hover:bg-blue-400 text-white w-10 h-auto p-2 rounded-full font-primary font-semibold text-xs flex
                     justify-center items-baselinetransform transition duration-500 group cursor-pointer">
-                    <FontAwesomeIcon icon={faEdit} className="w-5 m-auto"/>
-                    </button>
+                            <FontAwesomeIcon icon={faEdit} className="w-5 m-auto" />
+                        </button>
                     </Link>
 
                     <button onClick={() => handleDelete(row.id)} className="bg-red-500 ml-0 hover:bg-red-400 text-white w-10 h-auto p-2 rounded-full font-primary font-semibold text-xs flex
                     justify-center items-baselinetransform transition duration-500 group cursor-pointer">
-                    <FontAwesomeIcon icon={faTrash} className="w-5 m-auto"/>
+                        <FontAwesomeIcon icon={faTrash} className="w-5 m-auto" />
                     </button>
                 </div>
             ),
@@ -97,26 +109,29 @@ const Products = ({products}) => {
             }
         };
         return (
-            <FilterComponent onFilter={e => setFilterText(e.target.value)} onClear={handleClear} filterText={filterText}/>
-        );
-    }, [filterText]);
+            <FilterComponent
+                onFilter={e => setFilterText(e.target.value)}
+                onClear={handleClear}
+                filterText={filterText}
+                columns={availableCols}
+            />
+    );
+}, [filterText]);
 
- 
-    return (
-        <div className="min-h-80 max-w-12 my-4 sm:my-8 mx-auto w-full">
 
-             <div className="overflow-hidden">
-
-                <DataTable
-                    columns={columns}
-                    data={data}
-                    pagination
-                    subHeader
-                    subHeaderComponent={subHeaderComponentMemo}
-                    />
-            </div>
+return (
+    <div className="min-h-80 max-w-12 my-4 sm:my-8 mx-auto w-full">
+        <div className="overflow-hidden">
+            <DataTable
+                columns={columns}
+                data={data}
+                pagination
+                subHeader
+                subHeaderComponent={subHeaderComponentMemo}
+            />
         </div>
-    )
+    </div>
+)
 }
 
 
