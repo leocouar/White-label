065d28c47ab3d4b37e4ca8b775dadf7a5ada1f8d
unknown <valentimega@gmail.com>
2023-10-31 14:46:10 -0300

    dashboard

--- components/dashboard/MenuDashboard.js
+++ components/dashboard/MenuDashboard.js
@@ -1,32 +1,48 @@
+import { useState } from 'react';
+
 function MenuDashboard() {
+    const [vista, setVista] = useState(false);
+
+    const handleClick = (texto) => {
+        if (texto === 'Comercio') {
+            setVista('comercio');
+        } else if (texto === 'Productos') {
+            setVista('productos');
+        }
+    };
     return (
-        <div class="p-2 bg-gray w-60 flex flex-col hidden md:flex" id="sideNav">
-            <nav>
-                <a class="block text-gray-500 py-2.5 px-4 my-4 rounded transition duration-100 hover:bg-gradient-to-r hover:from-cyan-400 hover:to-cyan-300 hover:text-white" href="#">
-                    <i class="fas fa-home mr-2"></i>Productos
-                </a>
-                <a class="block text-gray-300 py-2.5 px-4 my-4 rounded transition duration-100 hover:bg-gradient-to-r hover:from-cyan-400 hover:to-cyan-300 hover:text-white" href="#">
-                    <i class="fas fa-file-alt mr-2"></i>Comercio
-                </a>
-                <a class="block text-gray-500 py-2.5 px-4 my-4 rounded transition duration-100 hover:bg-gradient-to-r hover:from-cyan-400 hover:to-cyan-300 hover:text-white" href="#">
-                    <i class="fas fa-users mr-2"></i>Usuarios
-                </a>
-                
-            </nav>
+        <>
+            <div>
+                <div class="p-2 bg-black-500 w-60 flex flex-col hidden md:flex" id="sideNav">
+                    <nav>         
+                            <div onClick={() => handleClick('Comercio')} class="block text-black-500 py-2.5 px-4 my-4 rounded transition hover:py-3.5 duration-200 hover:bg-gradient-to-r hover:from-cyan-400 hover:to-cyan-300 hover:text-gray cursor-pointer" >
+                                Comercio
+                            </div>
 
-
-            <a class="block text-gray-500 py-2.5 px-4 my-2 rounded transition duration-100 hover:bg-gradient-to-r hover:from-cyan-400 hover:to-cyan-300 hover:text-white mt-auto" href="#">
-                <i class="fas fa-sign-out-alt mr-2"></i>Cerrar sesión
-            </a>
-
-
-            <div class="bg-gradient-to-r from-cyan-300 to-cyan-500 h-px mt-2"></div>
-
-
-            
-
-        </div>
+                            <div onClick={() => handleClick('Productos')} class="block text-black-500 py-2.5 px-4 my-4 rounded transition duration-200 hover:bg-gradient-to-r hover:from-cyan-400 hover:to-cyan-300 hover:text-gray cursor-pointer">
+                                Productos
+                            </div>
+                    </nav>
+                    
+                    <div onClick={() => handleClick('Cerrar sesion')} class="block text-black-500 py-2.5 px-4 my-2 rounded transition duration-200 hover:bg-gradient-to-r hover:from-cyan-400 hover:to-cyan-300 hover:text-gray mt-auto cursor-pointer" >
+                        Cerrar sesión
+                    </div>
+                    
+                    <div class="bg-gradient-to-r from-cyan-300 to-cyan-500 h-px mt-2"></div>
+                </div>
+                {vista === 'comercio' && (
+                    <div className="mt-8 p-4 bg-white shadow-md rounded-md">
+                        Comercio
+                    </div>
+                )}
+                {vista === 'productos' && (
+                    <div className="mt-8 p-4 bg-white shadow-md rounded-md">
+                        Productos
+                    </div>
+                )}
+            </div>
+        </>
     )
 
 }
-export default MenuDashboard()
+export default MenuDashboard
--- pages/dashboard/[id]/index.js
+++ pages/dashboard/[id]/index.js
@@ -1,29 +1,12 @@
-import menuDashboard from "@/components/dashboard/MenuDashboard.js"
+import MenuDashboard from "@/components/dashboard/MenuDashboard.js"
 
-function dashboard() {
+
+function Dashboard() {
 
     return (
         <div>
-            <div class="p-2 bg-black-500 w-60 flex flex-col hidden md:flex" id="sideNav">
-                <nav>
-                    <a class="block text-black-500 py-2.5 px-4 my-4 rounded transition hover:py-3.5 duration-200 hover:bg-gradient-to-r hover:from-cyan-400 hover:to-cyan-300 hover:text-gray" href="#">
-                        <i class="fas fa-file-alt mr-2"></i>Comercio
-                    </a>
-
-                    <a class="block text-black-500 py-2.5 px-4 my-4 rounded transition duration-200 hover:bg-gradient-to-r hover:from-cyan-400 hover:to-cyan-300 hover:text-gray" href="#">
-                        <i class="fas fa-home mr-2"></i>Productos
-                    </a>
-                    <a class="block text-black-500 py-2.5 px-4 my-4 rounded transition duration-200 hover:bg-gradient-to-r hover:from-cyan-400 hover:to-cyan-300 hover:text-gray" href="#">
-                        <i class="fas fa-home mr-2"></i>Volver
-                    </a>
-                </nav>
-
-                <a class="block text-black-500 py-2.5 px-4 my-2 rounded transition duration-200 hover:bg-gradient-to-r hover:from-cyan-400 hover:to-cyan-300 hover:text-gray mt-auto" href="#">
-                    <i class="fas fa-sign-out-alt mr-2"></i>Cerrar sesión
-                </a>
-                <div class="bg-gradient-to-r from-cyan-300 to-cyan-500 h-px mt-2"></div>
-            </div>
+            <MenuDashboard/>
         </div>
     )
 }
-export default dashboard
+export default Dashboard
