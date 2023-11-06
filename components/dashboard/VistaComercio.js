38e0c5641650f7dedaa6e5b3db35e8a47bae8959
lautaro <lautaro.bordino@gmail.com>
2023-11-06 14:28:13 -0300

    campos actualizables

--- components/dashboard/VistaComercio.js
+++ components/dashboard/VistaComercio.js
@@ -5,22 +5,15 @@ const VistaComercio = ({commerceData}) => {
   // State to control the modal
   const [isModalOpen, setIsModalOpen] = useState(false);
  
-  useEffect(()=>{
-    setStoreToUpdate({
+  const [storeToUpdate, setStoreToUpdate] = useState(commerceData.store.store);
+  const [storeToShow, setStoreToShow] = useState(commerceData.store.store)
+  const [refreshData, setRefreshData] = useState(false)
+
+    useEffect(()=>{
+    setStoreToShow({
       ...storeToUpdate
   })
-  },1000)
-  // Commerce information (you can use your own data or fetch it from an API)
-
-  // State variables for edited values
-  const [storeToUpdate, setStoreToUpdate] = useState(commerceData.store.store);
-  // [newEmail, setNewEmail]
-  // const [newPhone, setNewPhone] = useState(commerceData.phone);
-  // const [newAddress, setNewAddress] = useState(commerceData.address);
-  // const [newMondayToFriday, setNewMondayToFriday] = useState(commerceData.businessHours.mondayToFriday);
-  // const [newSaturday, setNewSaturday] = useState(commerceData.businessHours.saturday);
-  // const [newSunday, setNewSunday] = useState(commerceData.businessHours.sunday);
-
+  },[refreshData])
   const handleEditClick = () => {
     // Open the modal when the "Edit" button is clicked
     setIsModalOpen(true);
@@ -38,7 +31,8 @@ const VistaComercio = ({commerceData}) => {
 
   const handleSave = () => {
     // Update the commerce information with the new values
-        updateStore(storeToUpdate)
+        updateStore(storeToUpdate);
+        refreshData === true ? setRefreshData(false) : setRefreshData(true);
     
     // Close the modal
     setIsModalOpen(false);
@@ -47,37 +41,28 @@ const VistaComercio = ({commerceData}) => {
 
   return (
     <div className="bg-blue-100 p-4 rounded-md shadow-md">
-      <h1 className="text-4xl font-semibold mb-4">{storeToUpdate.name}</h1>
+      <h1 className="text-4xl font-semibold mb-4">{storeToShow.name}</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
         {/* Display commerce information */}
         <div>
-          <h2 className="text-2xl font-semibold">"{storeToUpdate.description}"</h2>
-          <ul className="list-disc ml-5">
+          <h2 className="text-2xl font-semibold">"{storeToShow.description}"</h2>
+          <ul className="list-disc ml-5 space-y-2">
             <li>
-              <span className="font-medium">Email:</span> {storeToUpdate.email}
+              <span className="font-extrabold">Address:</span> {storeToShow.address}
             </li>
             <li>
-              <span className="font-medium">Phone:</span> {storeToUpdate.telephone}
-            </li>
-            <li>
-              <span className="font-medium">Address:</span> {storeToUpdate.address}
+              <span className="font-extrabold">Schedule:</span> {storeToShow.schedule}
             </li>
           </ul>
         </div>
         <div>
-          <h2 className="text-2xl font-semibold">Business Hours</h2>
-          <ul className="list-disc ml-5">
+          <h2 className="text-2xl font-semibold">Contact</h2>
+          <ul className="list-disc ml-5 space-y-2">
             <li>
-              <span className="font-medium">Monday - Friday:</span>{' '}
-              {/* {commerceData.businessHours.mondayToFriday} */}
+              <span className="font-extrabold">Phone:</span> {storeToShow.telephone}
             </li>
             <li>
-              <span className="font-medium">Saturday:</span> 
-              {/* {commerceData.businessHours.saturday} */}
-            </li>
-            <li>
-              <span className="font-medium">Sunday:</span> 
-              {/* {commerceData.businessHours.sunday} */}
+              <span className="font-extrabold">Email:</span> {storeToShow.email}
             </li>
           </ul>
         </div>
@@ -99,6 +84,17 @@ const VistaComercio = ({commerceData}) => {
       <div className="modal-container bg-white w-96 md:w-2/5 mx-auto p-6 rounded-lg shadow-lg z-20 max-h-96 overflow-y-auto">
       <h2 className="text-lg font-semibold mb-4">Edit Commerce Information</h2>
       <div className="mb-4">
+        <label className="block text-sm font-medium mb-2">Description:</label>
+        <input
+          type="text"
+          value={storeToUpdate?.description}
+          onChange={handleChange}
+          className="w-full px-3 py-2 border rounded-md"
+          id="description"
+          name="description"
+        />
+      </div>
+      <div className="mb-4">
         <label className="block text-sm font-medium mb-2">Email:</label>
         <input
           type="text"
@@ -109,51 +105,39 @@ const VistaComercio = ({commerceData}) => {
           name="email"
         />
       </div>
-      {/* <div className="mb-4">
+      <div className="mb-4">
         <label className="block text-sm font-medium mb-2">Phone:</label>
         <input
           type="text"
-          value={newPhone}
-          onChange={(e) => setNewPhone(e.target.value)}
+          value={storeToUpdate?.telephone}
+          onChange={handleChange}
           className="w-full px-3 py-2 border rounded-md"
+          id="telephone"
+          name="telephone"
         />
       </div>
       <div className="mb-4">
         <label className="block text-sm font-medium mb-2">Address:</label>
         <input
           type="text"
-          value={newAddress}
-          onChange={(e) => setNewAddress(e.target.value)}
+          value={storeToUpdate?.address}
+          onChange={handleChange}
           className="w-full px-3 py-2 border rounded-md"
+          id="address"
+          name="address"
         />
       </div>
       <div className="mb-4">
-        <label className="block text-sm font-medium mb-2">Monday - Friday:</label>
+        <label className="block text-sm font-medium mb-2">Schedule:</label>
         <input
           type="text"
-          value={newMondayToFriday}
-          onChange={(e) => setNewMondayToFriday(e.target.value)}
+          value={storeToUpdate?.schedule}
+          onChange={handleChange}
           className="w-full px-3 py-2 border rounded-md"
+          id="schedule"
+          name="schedule"
         />
       </div>
-      <div className="mb-4">
-        <label className="block text-sm font-medium mb-2">Saturday:</label>
-        <input
-          type="text"
-          value={newSaturday}
-          onChange={(e) => setNewSaturday(e.target.value)}
-          className="w-full px-3 py-2 border rounded-md"
-        />
-      </div>
-      <div className="mb-4">
-        <label className="block text-sm font-medium mb-2">Sunday:</label>
-        <input
-          type="text"
-          value={newSunday}
-          onChange={(e) => setNewSunday(e.target.value)}
-          className="w-full px-3 py-2 border rounded-md"
-        />
-      </div> */}
       <button
         onClick={handleSave}
         className="bg-blue-500 text-white px-4 py-2 rounded-md mr-2"
