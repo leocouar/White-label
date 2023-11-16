11d4826b1a1dd2a0d2e13b45b134b8fb476325fd
Leonardo <leonardo.couar@gmail.com>
2023-11-16 15:00:59 -0300

    NextAuth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -60,7 +60,6 @@ const options = {
             return user;
         },
         async session(session,user, token) {
-            session.user = token.user;
             return session;
         },
         async jwt(token, user) {
