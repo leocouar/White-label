9e02cafb534b0dd6147ecf9f055f07fbf217c1a0
unknown <valentimega@gmail.com>
2023-11-21 15:14:55 -0300

    lista de comercio

--- components/products/CommerceBanner.js
+++ components/products/CommerceBanner.js
@@ -1,18 +1,28 @@
-
+import { useRouter } from "next/router";
+import Dashboard from "pages/dashboard/[id]";
   
 function Banner(stores) {
+  const router = useRouter();
+  const handleCardClick = (stores) =>{
+    const page = router.pathname
 
+    if(page === '/stores/list'){
+      router.push(`/dashboard/${stores.id}`)
+    } else if(page === '/'){
+      router.push(`/commerce/${stores.id}`)
+    }
+  }
     return (
 <div className="bg-white max-h-96 w-full">
       <div className="mx-auto px-2 sm:px-4">
         <div className="mx-auto max-h-96 py-4 sm:py-8 flex flex-nowrap overflow-x-auto">
-          {stores.stores.map((store, index) => (
+          {stores.stores && stores.stores.map((store, index) => (
             <div className="flex-shrink-0 mx-3 max-w-full" key={index}>
               <div className="bg-gray-200 relative mb-4 h-auto w-36 lg:w-48 border-4 border-gray-200 shadow-lg rounded-lg group-hover:opacity-75 sm:w-40 transition-transform duration-300 transform origin-center hover:scale-110">
-                <a href={`commerce/${store.id}`}>
-                  <img src={store.logo.link} alt={store.name} className="rounded" />
-                  <h1 className="flex truncate justify-center mt-4 font-primary hover:text-clip">{store.name}</h1>
-                </a>
+              <button onClick={() => handleCardClick(store)}>
+                    <img src={store.logo.link} alt={store.name} className="rounded" />
+                    <h1 className="flex truncate justify-center mt-4 font-primary hover:text-clip">{store.name}</h1>
+                  </button>
               </div>
             </div>
           ))}
--- pages/stores/list.js
+++ pages/stores/list.js
@@ -2,80 +2,29 @@ import NewStore from "@/components/stores/NewStore";
 import { getSession, getCsrfToken, signIn } from "next-auth/react";
 import StoreHeading from "@/components/StoreHeading";
 import ListStores from "@/components/stores/ListStore";
+import Banner from '@/components/products/CommerceBanner.js';
+import { findAllStores } from "services/storeService";
 
-
-const ListStore = () => {
-  const data= [
-    {
-        "id": 1179,
-        "name": "PablitosHause",
-        "description": "Moda y accesorios",
-        "email": null,
-        "telephone": null,
-        "address": null,
-        "logo": null
-    },
-    {
-        "id": 1182,
-        "name": "adminStore",
-        "description": "un storer de admin para admins",
-        "email": "admin@admin.com",
-        "telephone": "2314000000",
-        "address": "Alsina 11",
-        "logo": null
-    },
-    {
-        "id": 1173,
-        "name": "empresa8",
-        "description": "sao",
-        "email": null,
-        "telephone": null,
-        "address": null,
-        "logo": null
-    },
-    {
-        "id": 1174,
-        "name": "empresa9",
-        "description": "sao",
-        "email": null,
-        "telephone": null,
-        "address": null,
-        "logo": null
-    },
-    {
-        "id": 1177,
-        "name": "Mi Tienda numero 2",
-        "description": "Descripción de la tienda",
-        "email": null,
-        "telephone": null,
-        "address": null,
-        "logo": null
-    },
-    {
-        "id": 1175,
-        "name": "Mi Tienda",
-        "description": "Descripción de la tienda",
-        "email": null,
-        "telephone": null,
-        "address": null,
-        "logo": null
-    }
-]
-    return(
-        <div className="min-h-screen">
-            <StoreHeading title="Tus Comercios"/>
-            <ListStores stores={data}/>
-        </div>
-    )
-}
-export async function getServerSideProps(context) {
+function StoreList({stores}){
+  return(
+    <div className="w-full h-full">
+      <StoreHeading title="Tus Comercios"/>
+              <div className='flex justify-center'>
+                <Banner stores={stores}/>
+              </div>
+            </div>
+  )
+ 
+  }
+  export async function getServerSideProps(context) {
     const session = await getSession(context);
-  
+    const stores  = await findAllStores();
+
     if (!session) {
       return {
         redirect: {
           destination: "/", // Redirigir al dashboard si está autenticado
-          permanent: false,
+          permanent: false,          
         },
       };
     }
@@ -83,7 +32,9 @@ export async function getServerSideProps(context) {
     return {
       props: {
         csrfToken: await getCsrfToken(context),
+        stores
       },
     };
+    
   }
-export default ListStore;
+export default StoreList;
