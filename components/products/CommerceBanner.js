ac99b9fb00f8863a290ad60809f8841385da2333
unknown <valentimega@gmail.com>
2023-11-21 16:00:02 -0300

    lista de comercios

--- components/products/CommerceBanner.js
+++ components/products/CommerceBanner.js
@@ -1,17 +1,10 @@
 import { useRouter } from "next/router";
+import Link from "next/link";
 import Dashboard from "pages/dashboard/[id]";
   
 function Banner(stores) {
   const router = useRouter();
-  const handleCardClick = (stores) =>{
-    const page = router.pathname
-
-    if(page === '/stores/list'){
-      router.push(`/dashboard/${stores.id}`)
-    } else if(page === '/'){
-      router.push(`/commerce/${stores.id}`)
-    }
-  }
+  
     return (
 <div className="bg-white max-h-96 w-full">
       <div className="mx-auto px-2 sm:px-4">
@@ -19,10 +12,20 @@ function Banner(stores) {
           {stores.stores && stores.stores.map((store, index) => (
             <div className="flex-shrink-0 mx-3 max-w-full" key={index}>
               <div className="bg-gray-200 relative mb-4 h-auto w-36 lg:w-48 border-4 border-gray-200 shadow-lg rounded-lg group-hover:opacity-75 sm:w-40 transition-transform duration-300 transform origin-center hover:scale-110">
-              <button onClick={() => handleCardClick(store)}>
-                    <img src={store.logo.link} alt={store.name} className="rounded" />
-                    <h1 className="flex truncate justify-center mt-4 font-primary hover:text-clip">{store.name}</h1>
-                  </button>
+              <Link
+                    href={
+                      router.pathname === '/stores/list'
+                        ? `/dashboard/${store.id}`
+                        : 
+                        `/commerce/${store.id}`
+                      
+                    }
+                  >
+                    
+                      <img src={store?.logo?.link} alt={store.name} className="rounded" />
+                      <h1 className="flex truncate justify-center mt-4 font-primary hover:text-clip">{store.name}</h1>
+                    
+                  </Link>
               </div>
             </div>
           ))}
