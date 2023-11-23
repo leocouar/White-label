bc2f146577099d1c47830eca0777ef418e4cf9d0
lautaro <lautaro.bordino@gmail.com>
2023-11-23 15:48:29 -0300

    hotfix del hotfix banner

--- components/products/CommerceBanner.js
+++ components/products/CommerceBanner.js
@@ -34,7 +34,7 @@ function Banner(stores) {
       <div className="bg-white max-h-96 w-full overflow-x-auto scrollbar">
       <div className="mx-auto px-2 sm:px-4">
         <div className="mx-auto max-h-96 py-4 sm:py-8 flex flex-nowrap overflow-x-auto scrollbar">
-          {stores.stores && stores.map((store, index) => (
+          {stores.stores && stores.stores.map((store, index) => (
             <div className="flex-shrink-0 mx-3 max-w-full" key={index}>
               <div className="bg-gray-200 relative mb-4 h-auto w-36 lg:w-48 border-4 border-gray-200 shadow-lg rounded-lg group-hover:opacity-75 sm:w-40 transition-transform duration-300 transform origin-center hover:scale-110">
               <Link
@@ -47,7 +47,7 @@ function Banner(stores) {
                     }
                   >
                       <img 
-                      src={store?.store?.logo?.link || DefaultImage}
+                      src={store?.logo?.link || DefaultImage}
                       alt={store.name}
                       className="rounded"
                        />
