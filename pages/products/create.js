bbb883c9b0313c4ee3dbd3d6a8eb4de6452ab5a6
Leonardo <leonardo.couar@gmail.com>
2023-11-29 15:47:20 -0300

    NewProduct

--- pages/products/create.js
+++ pages/products/create.js
@@ -2,11 +2,11 @@ import NewProduct from "@/components/products/NewProduct";
 import StoreHeading from "@/components/StoreHeading";
 import withAuthorization from 'components/withAuthorization';
 
-const Create = ({brands, categories, sizes}) => {
+const Create = () => {
     return(
         <div className="min-h-screen">
             <StoreHeading title="Nuevo Articulo"/>
-            <NewProduct brands={brands} categories={categories} sizes={sizes}/>
+            <NewProduct/>
         </div>
     )
 }
