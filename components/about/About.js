e4a2f54cddc95753f6232fcf453b209e6f3de783
lautaro <lautaro.bordino@gmail.com>
2023-12-01 14:52:21 -0300

    about2+ camaraimage

--- components/about/About.js
+++ components/about/About.js
@@ -1,8 +1,10 @@
 const About = () => {
+    const image = ['/images/camara-frente.jpeg']
     return(
+        <div className="bg-gray-200">
         <div className="max-w-4xl mx-auto mt-10 p-6 bg-white shadow-md rounded-lg">
             <h2 className="text-3xl text-blue-700 font-semibold mb-6">NUESTROS PILARES</h2>
-            <p className="text-lg mb-6">
+            <p className="text-lg mb-6 text-justify">
                 La Cámara Comercial e Industrial del partido de Bolívar es una entidad de representación gremial,
                 comprometida con la defensa del comercio, la industria y los servicios locales, que,
                 mediante un trabajo mancomunado con entidades afines, acompaña el desarrollo y la sostenibilidad del sector,
@@ -29,6 +31,12 @@ const About = () => {
                         <li>Tu voz Cuenta</li>
                     </ul>
                 </div>
+            </div>
+        </div>
+            <div className="flex justify-center w-full">
+                <img
+                src={`${image}`}
+                className="w-full"></img>
             </div>
         </div>
     );
