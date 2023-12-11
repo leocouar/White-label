a607e9ddb862ad14184d9e9021234eb241963a53
Alan Medina <medinaalangabriel@gmail.com>
2023-12-11 13:34:53 -0300

    Update de admin para catalogo - Pagina de Comercios agregada

--- pages/admin.js
+++ pages/admin.js
@@ -4,8 +4,9 @@ import withAuthorization from 'components/withAuthorization';
 const Admin = () => {
     return (
 
-            <div className="antialiased bg-blue-200 w-full font-ssans flex flex-wrap">
-                <div className="flex flex-wrap m-2 w-full justify-self-center grid grid-cold-1 md:grid-cols-2 container mx-auto">
+        <div className="antialiased bg-blue-200 w-full font-ssans flex flex-wrap">
+            <div className="flex flex-wrap m-2 w-full justify-self-center grid grid-cold-1 md:grid-cols-2 container mx-auto">
+                {/*
                     <Link legacyBehavior href="/bills">
                         <div className="pl-1 my-4 w-80 h-24 flex lg:w-3/4 justify-self-center bg-green-400 rounded-lg shadow-md cursor-pointer">
                             <div className="flex w-full h-full py-2 px-4 bg-white rounded-lg justify-between">
@@ -21,6 +22,8 @@ const Admin = () => {
                             </div>
                         </div>
                     </Link>
+                    */}
+                {/*
                     <Link legacyBehavior href="/stock">
                         <div className="pl-1 my-4 w-80 h-24 lg:w-3/4 justify-self-center bg-blue-500 rounded-lg shadow-md cursor-pointer">
                             <div className="flex w-full h-full py-2 px-4 bg-white rounded-lg justify-between">
@@ -36,36 +39,38 @@ const Admin = () => {
                             </div>
                         </div>
                     </Link>
-                    <Link legacyBehavior href="/products">
-                        <div className="pl-1 my-4 w-80 h-24 lg:w-3/4 justify-self-center bg-yellow-400 rounded-lg shadow-md cursor-pointer">
-                            <div className="flex w-full h-full py-2 px-4 bg-white rounded-lg justify-between">
-                                <div className="my-auto">
-                                    <p className="font-bold">Articulos</p>
-                                    <p className="text-lg">Detalle de los articulos</p>
-                                </div>
-                                <div className="my-auto">
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z" />
-                                    </svg>
-                                </div>
+                    */}
+                <Link legacyBehavior href="/products">
+                    <div className="pl-1 my-4 w-80 h-24 lg:w-3/4 justify-self-center bg-yellow-400 rounded-lg shadow-md cursor-pointer">
+                        <div className="flex w-full h-full py-2 px-4 bg-white rounded-lg justify-between">
+                            <div className="my-auto">
+                                <p className="font-bold">Articulos</p>
+                                <p className="text-lg">Detalle de los articulos</p>
+                            </div>
+                            <div className="my-auto">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z" />
+                                </svg>
                             </div>
                         </div>
-                    </Link>
-                    <Link legacyBehavior href="/users">
-                        <div className="pl-1 my-4 w-80 h-24 lg:w-3/4 justify-self-center bg-yellow-400 rounded-lg shadow-md cursor-pointer">
-                            <div className="flex w-full h-full py-2 px-4 bg-white rounded-lg justify-between">
-                                <div className="my-auto">
-                                    <p className="font-bold">Usuarios</p>
-                                    <p className="text-lg">Detalle de los usuario del cargados</p>
-                                </div>
-                                <div className="my-auto">
+                    </div>
+                </Link>
+                <Link legacyBehavior href="/users">
+                    <div className="pl-1 my-4 w-80 h-24 lg:w-3/4 justify-self-center bg-yellow-400 rounded-lg shadow-md cursor-pointer">
+                        <div className="flex w-full h-full py-2 px-4 bg-white rounded-lg justify-between">
+                            <div className="my-auto">
+                                <p className="font-bold">Usuarios</p>
+                                <p className="text-lg">Detalle de los usuario del cargados</p>
+                            </div>
+                            <div className="my-auto">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z" />
-                                </svg>       
-                                </div>
+                                </svg>
                             </div>
                         </div>
-                    </Link>
+                    </div>
+                </Link>
+                {/*
                     <Link legacyBehavior href="/checkout">
                         <div className="pl-1 my-4 w-80 h-24 lg:w-3/4 justify-self-center bg-green-400 rounded-lg shadow-md cursor-pointer">
                             <div className="flex w-full h-full py-2 px-4 bg-white rounded-lg justify-between">
@@ -81,38 +86,39 @@ const Admin = () => {
                             </div>
                         </div>
                     </Link>
-                    <Link legacyBehavior href="/brand">
-                        <div className="pl-1 my-4 w-80 h-24 lg:w-3/4 justify-self-center bg-blue-500 rounded-lg shadow-md cursor-pointer">
-                            <div className="flex w-full h-full py-2 px-4 bg-white rounded-lg justify-between">
-                                <div className="my-auto">
-                                    <p className="font-bold">Marcas</p>
-                                    <p className="text-lg">Detalle de las marcas de articulos</p>
-                                </div>
-                                <div className="my-auto">
+                    */}
+                <Link legacyBehavior href="/brand">
+                    <div className="pl-1 my-4 w-80 h-24 lg:w-3/4 justify-self-center bg-blue-500 rounded-lg shadow-md cursor-pointer">
+                        <div className="flex w-full h-full py-2 px-4 bg-white rounded-lg justify-between">
+                            <div className="my-auto">
+                                <p className="font-bold">Marcas</p>
+                                <p className="text-lg">Detalle de las marcas de articulos</p>
+                            </div>
+                            <div className="my-auto">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4" />
                                 </svg>
-                                </div>
                             </div>
                         </div>
-                    </Link>
+                    </div>
+                </Link>
 
-                    <Link legacyBehavior href="/category">
-                        <div className="pl-1 my-4 w-80 h-24 lg:w-3/4 justify-self-center bg-blue-500 rounded-lg shadow-md cursor-pointer">
-                            <div className="flex w-full h-full py-2 px-4 bg-white rounded-lg justify-between">
-                                <div className="my-auto">
-                                    <p className="font-bold">Categorias</p>
-                                    <p className="text-lg">Detalle de las categorias de articulos</p>
-                                </div>
-                                <div className="my-auto">
+                <Link legacyBehavior href="/category">
+                    <div className="pl-1 my-4 w-80 h-24 lg:w-3/4 justify-self-center bg-blue-500 rounded-lg shadow-md cursor-pointer">
+                        <div className="flex w-full h-full py-2 px-4 bg-white rounded-lg justify-between">
+                            <div className="my-auto">
+                                <p className="font-bold">Categorias</p>
+                                <p className="text-lg">Detalle de las categorias de articulos</p>
+                            </div>
+                            <div className="my-auto">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4" />
                                 </svg>
-                                </div>
                             </div>
                         </div>
-                    </Link>
-
+                    </div>
+                </Link>
+                {/*
                     <Link legacyBehavior href="/reports">
                         <div className="pl-1 my-4 w-80 h-24 lg:w-3/4 justify-self-center bg-green-400 rounded-lg shadow-md cursor-pointer">
                             <div className="flex w-full h-full py-2 px-4 bg-white rounded-lg justify-between">
@@ -128,7 +134,8 @@ const Admin = () => {
                             </div>
                         </div>
                     </Link>
-
+                    */}
+                {/*
                     <Link legacyBehavior href="/proveedores">
                         <div className="pl-1 my-4 w-80 h-24 lg:w-3/4 justify-self-center bg-green-400 rounded-lg shadow-md cursor-pointer">
                             <div className="flex w-full h-full py-2 px-4 bg-white rounded-lg justify-between">
@@ -144,25 +151,38 @@ const Admin = () => {
                             </div>
                         </div>
                     </Link>
-
-                    <Link legacyBehavior href="/size">
-                        <div className="pl-1 my-4 w-80 h-24 lg:w-3/4 justify-self-center bg-blue-500 rounded-lg shadow-md cursor-pointer">
-                            <div className="flex w-full h-full py-2 px-4 bg-white rounded-lg justify-between">
-                                <div className="my-auto">
-                                    <p className="font-bold">Talles</p>
-                                    <p className="text-lg">Detalle de los talles de articulos</p>
-                                </div>
-                                <div className="my-auto">
+                */}
+                <Link legacyBehavior href="/size">
+                    <div className="pl-1 my-4 w-80 h-24 lg:w-3/4 justify-self-center bg-blue-500 rounded-lg shadow-md cursor-pointer">
+                        <div className="flex w-full h-full py-2 px-4 bg-white rounded-lg justify-between">
+                            <div className="my-auto">
+                                <p className="font-bold">Talles</p>
+                                <p className="text-lg">Detalle de los talles de articulos</p>
+                            </div>
+                            <div className="my-auto">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4" />
                                 </svg>
-                                </div>
                             </div>
                         </div>
-                    </Link>
-                    
-                    
-                </div>
+                    </div>
+                </Link>
+                <Link legacyBehavior href="/stores">
+                    <div className="pl-1 my-4 w-80 h-24 lg:w-3/4 justify-self-center bg-blue-500 rounded-lg shadow-md cursor-pointer">
+                        <div className="flex w-full h-full py-2 px-4 bg-white rounded-lg justify-between">
+                            <div className="my-auto">
+                                <p className="font-bold">Comercios</p>
+                                <p className="text-lg">Detalle de los comercios.</p>
+                            </div>
+                            <div className="my-auto">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4" />
+                                </svg>
+                            </div>
+                        </div>
+                    </div>
+                </Link>
+            </div>
         </div>
     )
 
