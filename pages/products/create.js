dc36027f55f16b58e2b14b10760322859ecfadf0
Alan Medina <medinaalangabriel@gmail.com>
2024-01-12 17:01:43 -0300

    Cancelar funciona, Tambien se validan los campos apropiadamente.

--- pages/products/create.js
+++ pages/products/create.js
@@ -1,13 +1,20 @@
 import SEO from "@/components/SEO";
 import NewProduct from "@/components/products/NewProduct";
 import withAuthorization from 'components/withAuthorization';
+import { useRouter } from "next/router";
 
 const Create = () => {
+    const router = useRouter();
+
+    const onCancel = () =>{
+        router.push("/products");
+    }
+
     return(
         <>
         <SEO title="Agregar nuevo producto" />
         <div className="min-h-screen pt-4">
-            <NewProduct admin={true}/>
+            <NewProduct onCancel={onCancel} admin={true}/>
         </div>
         </>
     )
