1fd7eb9e579b72d7d4c090439f443842286cba76
Alan Medina <medinaalangabriel@gmail.com>
2024-01-15 17:13:33 -0300

    Mejoras realizadas en la seleccion de propietarios para Agregar Comercio.

--- pages/stores/create.js
+++ pages/stores/create.js
@@ -2,18 +2,28 @@ import NewStore from "@/components/stores/NewStore";
 import StoreHeading from "@/components/StoreHeading";
 import withAuthorization from 'components/withAuthorization';
 import SEO from "@/components/SEO";
+import { findAll } from 'services/userService';
 
-
-const CreateStore = () => {
+const CreateStore = ({users}) => {
     return(
         <>
         <SEO title="Agregar un nuevo comercio" />
         <div className="min-h-screen">
             <StoreHeading title="Nuevo Comercio"/>
-            <NewStore/>
+            <NewStore users={users}/>
         </div>
         </>
     )
+}
+
+export async function getServerSideProps() {
+    const users = await findAll();
+
+    return {
+        props: {
+            users
+        }
+    };
 }
 
 export default CreateStore;
