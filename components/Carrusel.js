6ff59f82b826b0f78a27e8e197e0f606a32ffa42
EnzoCepeda <enzoocobhc@hotmail.com.ar>
2024-01-18 19:40:55 +0100

    CarouselResponsiveFix

--- components/Carrusel.js
+++ components/Carrusel.js
@@ -12,19 +12,22 @@ function Carousel() {
   const [initialHeight, setInitialHeight] = useState(null);
   const [isVisible, setIsVisible] = useState(false);
 
+  
   useEffect(() => {
     const container = containerRef.current;
+  
     if (container) {
-      const newimgs = container.getElementsByTagName('img');
-      setImgs(newimgs);
+      const newImgs = container.getElementsByTagName('img');
+      setImgs(newImgs); // No necesitas convertir HTMLCollection a un array
     }
   }, [containerRef]);
-
+  
   useEffect(() => {
     const updateWindowDimensions = () => {
       let height;
-
+  
       setImages(largeImages);
+  
       if (window.innerWidth >= 1024) {
         height = window.innerHeight - 60;
       } else if (window.innerWidth >= 768) {
@@ -33,68 +36,57 @@ function Carousel() {
         setImages(responsiveImages);
         height = window.innerHeight - 88;
       }
-
+  
       setWindowDimensions([height, window.innerWidth * 3]);
-
+  
+      const container = containerRef.current;
+      const newImgs = container.getElementsByTagName('img');
+      setImgs(newImgs);
+  
       imgs[1] && imgs[1].scrollIntoView({ block: 'nearest', inline: 'start' });
-    };
-
-    const handleVisibilityChange = (entries) => {
-      entries.forEach(entry => {
-        setIsVisible(entry.isIntersecting);
+  
+      // Añadir lógica para ajustar el height de las imágenes en pantallas pequeñas
+      const imgHeight = window.innerWidth < 768 ? 'auto' : `${initialHeight}px`;
+  
+      Array.from(newImgs).forEach((img) => {
+        img.style.height = imgHeight;
       });
     };
-
-    const observer = new IntersectionObserver(handleVisibilityChange, {
-      root: null, // use the viewport as the root
-      rootMargin: '0px', // no margin
-      threshold: 0.5, // 50% of the target element must be visible
-    });
-
-    if (containerRef.current) {
-      observer.observe(containerRef.current);
-    }
-
-    window.addEventListener('resize', updateWindowDimensions);
+  
+    // Llama a la función de actualización inicial
+    updateWindowDimensions();
+  
     updateWindowDimensions();
 
-    setInitialHeight(windowDimensions[0]);
+    const intervalId = setInterval(() => {
+      const nextImgIndex = currentImage !== images.length - 1 ? currentImage + 1 : 0;
+      setCurrentImage(nextImgIndex);
+      imgs[2].setAttribute('src', `${images[nextImgIndex]}`);
+
+      const temp = imgs[0].cloneNode(true);
+
+      for (let i = 0; i < imgs.length - 1; i++) {
+        const nextElement = imgs[i + 1].cloneNode(true);
+        imgs[i].parentNode.replaceChild(nextElement, imgs[i]);
+      }
+      imgs[imgs.length - 1].parentNode.replaceChild(temp, imgs[imgs.length - 1]);
+      containerRef.current.scrollTo({ left: 0 });
+
+      imgs[1].scrollIntoView({
+        behavior: 'smooth',
+        block: 'nearest',
+        inline: 'start',
+        height: '200',
+      });
+    }, 5000);
+
+    window.addEventListener('resize', updateWindowDimensions);
 
     return () => {
+      clearInterval(intervalId);
       window.removeEventListener('resize', updateWindowDimensions);
-      observer.disconnect();
     };
-  }, [imgs, initialHeight]);
-
-  useEffect(() => {
-    if (isVisible) {
-      const intervalId = setInterval(() => {
-        const nextImgIndex = currentImage !== images.length - 1 ? currentImage + 1 : 0;
-        setCurrentImage(nextImgIndex);
-        imgs[2].setAttribute('src', `${images[nextImgIndex]}`);
-
-        const temp = imgs[0].cloneNode(true);
-
-        for (let i = 0; i < imgs.length - 1; i++) {
-          const nextElement = imgs[i + 1].cloneNode(true);
-          imgs[i].parentNode.replaceChild(nextElement, imgs[i]);
-        }
-        imgs[imgs.length - 1].parentNode.replaceChild(temp, imgs[imgs.length - 1]);
-        containerRef.current.scrollTo({ left: 0 });
-
-        imgs[1].scrollIntoView({
-          behavior: 'smooth',
-          block: 'nearest',
-          inline: 'start',
-          height: '200',
-        });
-      }, 3000);
-
-      return () => {
-        clearInterval(intervalId);
-      };
-    }
-  }, [isVisible, currentImage, imgs, images]);
+  }, [containerRef, imgs, initialHeight, largeImages, responsiveImages, currentImage, images]);
 
   const spin = (backwards) => {
     const numimgs = imgs.length;
@@ -145,26 +137,13 @@ function Carousel() {
   const arrowStyle = 'absolute text-white text-2xl z-10 bg-white h-10 w-10 rounded-full opacity-75 flex items-center justify-center';
 
   const sliderControl = isLeft => (
-    <button
-      type="button"
-      onClick={isLeft ? () => spin(true) : () => spin()}
-      className={`${arrowStyle} ${isLeft ? 'left-2' : 'right-2'}`}
-      style={{ top: '40%' }}
-    >
-      <span role="img" aria-label={`Arrow ${isLeft ? 'left' : 'right'}`} className={'bg-slate-900'}>
-        {isLeft ? (
-          <svg xmlns="http://www.w3.org/2000/svg" height="16" width="14" viewBox="0 0 448 512"><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z"/></svg>
-        ) : (
-            <svg xmlns="http://www.w3.org/2000/svg" height="16" width="14" viewBox="0 0 448 512"><path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"/></svg>
-        )}
-      </span>
-    </button>
+  <></>
   );
 
   const positions = [1, 2, 3];
   return (
 
-    <div style={heightAdjust} className="relative">
+    <div style={{heightAdjust}}className="relative">
       <div id="container" ref={containerRef} className="w-full overflow-hidden">
         {sliderControl(true)}
         <div className="flex" style={{ width: `${windowDimensions[1]}px` }}>
