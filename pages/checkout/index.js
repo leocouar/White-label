75efaada5394e987825b21c6a95a2959ff21a30c
EnzoCepeda <enzoocobhc@hotmail.com.ar>
2024-01-24 19:55:29 +0100

    Checkout Titles

--- pages/checkout/index.js
+++ pages/checkout/index.js
@@ -3,14 +3,18 @@ import React, {useEffect, useState} from "react";
 import {findAll} from "../../services/checkoutService";
 import PageTitle from "@/components/PageTitle";
 import withAuthorization from 'components/withAuthorization';
+import SEO from "@/components/SEO";
 
 const Index = () => {
 
     return (
+        <>
+        <SEO title="Tus Compras" />
         <div className="mx-auto max-w-6xl">
-            <PageTitle text={`Checkout`} />
+            <PageTitle text={`Tus Compras`} />
             <List/>
         </div>
+        </>
     )
 }
 
