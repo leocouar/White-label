c21f060186a08e4b16b68f6a9fddb4dacad6d591
Alan Medina <medinaalangabriel@gmail.com>
2024-02-06 17:17:34 -0300

    Se añadieron los endpoints para manipular favoritos.

---
+++ services/favoriteService.js
@@ -0,0 +1,45 @@
+import axios from 'axios';
+
+
+export async function save(favorite) {
+    const fetchUrl = `${process.env.NEXT_PUBLIC_BACKEND_SERVICE}/favorite`;
+    try {
+        let response = await axios.post(fetchUrl, favorite);
+        return response;
+    } catch (error) {
+        console.error(error)
+        throw new Error("Could not create favorite!");
+    }
+}
+
+export async function get(id) {
+    const fetchUrl = `${process.env.NEXT_PUBLIC_BACKEND_SERVICE}/favorite/${id}`;
+    try {
+        let response = await axios.get(fetchUrl);
+        return response;
+    } catch (error) {
+        console.error(error)
+        throw new Error("Could not obtain favorite!");
+    }
+}
+
+export async function deleteFav(id) {
+    const fetchUrl = `${process.env.NEXT_PUBLIC_BACKEND_SERVICE}/favorite/${id}`;
+    try {
+        let response = await axios.delete(fetchUrl);
+        return response;
+    } catch (error) {
+        console.error(error)
+        throw new Error("Could not delete favorite!");
+    }
+}
+
+export async function findByUser(page = 0, name = "", asc=true, username){
+    const fetchUrl = `${process.env.NEXT_PUBLIC_BACKEND_SERVICE}/favorite/user/${username}?asc=${asc}&page=${page}&size=${size}`;
+    try {
+        const response = await axios.get(fetchUrl);
+        return response.data;
+    } catch (error) {
+        return []
+    }
+}
