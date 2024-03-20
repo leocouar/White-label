f1e49397c769be10bee2d6c47be2130636348e08
EnzoCepeda <enzoocobhc@hotmail.com.ar>
2024-03-20 15:23:25 -0300

    Se rediseño el boton de whatsapp

--- components/whatsapp/WhatsAppButton.js
+++ components/whatsapp/WhatsAppButton.js
@@ -12,11 +12,14 @@ const WhatsAppButton = ({ phoneNumber, message, actionPostRedirect }) => {
     return (
         <div>
             <button
-                className='h-11 pt-1 justify-center items-center pb-1 font-primary font-semibold flex items-center text-white bg-green-600 hover:bg-green-700 border-black border rounded-lg p-2 text-md w-full'
+                className='h-12 pt-1 justify-center items-center pb-1 font-semibold flex items-center text-white bg-gray-400 hover:bg-green-700 rounded-2xl p-2 text-md w-full'
                 onClick={handleWhatsAppClick}
             >
-                <img src={logo.src} style={{ width: "2rem" }} alt="WhatsApp Logo" className="" />
-                Consultar por WhatsApp
+                <img src={logo.src} style={{ width: "3rem" }} alt="WhatsApp Logo" className="" />
+                <div className='flex flex-col'>
+                <a className='text-xs'>Escribenos en</a>
+                <a className='text-lg ml-2'>WhatsApp</a>
+                </div>
             </button>
         </div>
 
